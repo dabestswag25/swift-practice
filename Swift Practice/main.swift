39bb27d65e2655d37d2b516e9f4b657364068235
dabestswag25 <1036888@etusd.org>
2017-10-27 16:26:44 -0700

    Initial commit

---
+++ Swift Practice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Practice
+//
+//  Created by UCode on 10/27/17.
+//  Copyright © 2017 Preston Tran. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
